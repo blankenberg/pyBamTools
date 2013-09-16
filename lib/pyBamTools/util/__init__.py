e19b8c3ead81ce04bce7a1a188ecb845dfc08397
Daniel Blankenberg <blankenberg@users.noreply.github.com>
2013-09-16 10:12:42 -0400

    More integer checking fixes

--- lib/pyBamTools/util/__init__.py
+++ lib/pyBamTools/util/__init__.py
@@ -8,3 +8,9 @@ NULL_CHAR = '\x00'
 MAX_NEG_INT = -sys.maxint
 
 NUMPY_DTYPES = dict( uint8=numpy.uint8, uint16=numpy.uint16, uint32=numpy.uint32, uint64=numpy.uint64 )
+
+INTEGER_TYPES = tuple( NUMPY_DTYPES.values() + [ int, long ] )
+
+def is_integer( value ):
+    """Check if value is a valid unsigned int, int, or long"""
+    return isinstance( value, INTEGER_TYPES )

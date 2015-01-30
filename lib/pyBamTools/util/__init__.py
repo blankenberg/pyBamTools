40a6bfa4e3c607968fa3b20ca580a25160dd88ca
Daniel Blankenberg <dan@bx.psu.edu>
2015-01-30 16:47:04 -0500

    Add a helper function to estimate the number of bytes to be used by numpy arrays.

--- lib/pyBamTools/util/__init__.py
+++ lib/pyBamTools/util/__init__.py
@@ -50,11 +50,39 @@ def guess_numpy_dtypes_from_idxstats( bams, default=None ):
                 rval[i] = cur_val + val
     for i, val in enumerate( rval ):
         if val is None:
-            # Default to default or largest size
-            dtype = default or NUMPY_DTYPE_SIZES[-1][0]
+            # Default to default
+            dtype = default
         else:
             for ( dtype, size ) in NUMPY_DTYPE_SIZES:
                 if val < size:
                     break
         rval[i] = dtype
     return rval
+
+def guess_array_memory_usage( bam_readers, dtype, use_strand=False ):
+    """Returns an estimate for the maximum amount of memory to be consumed by numpy arrays."""
+    ARRAY_COUNT = 5
+    if not isinstance( bam_readers, list ):
+        bam_readers = [ bam_readers ]
+    if isinstance( dtype, basestring ):
+        dtype = NUMPY_DTYPES.get( dtype, None )
+    use_strand = use_strand + 1 #if false, factor of 1, if true, factor of 2
+    dtypes = guess_numpy_dtypes_from_idxstats( bam_readers, default=None )
+    if not [ dt for dt in dtypes if dt is not None ]:
+        dtypes = guess_numpy_dtypes_from_idxstats( bam_readers, default=dtype )
+    read_groups = []
+    no_read_group = False
+    max_ref_size = 0
+    for bam in bam_readers:
+        rgs = bam.get_read_groups()
+        if rgs:
+            for rg in rgs:
+                if rg not in read_groups:
+                    read_groups.append( rg )
+        else:
+            no_read_group = True
+        for i, ( name, length ) in enumerate( bam.get_references() ):
+            if dtypes[i] is not None:
+                max_ref_size = max( max_ref_size, ( length * dtypes[i]().nbytes) )
+    read_groups = len( read_groups ) + no_read_group
+    return max_ref_size * ARRAY_COUNT * use_strand * read_groups
